dcc57d79cfae26d4ddcb9118bf0665f74c1a379b
alope <pablitojun@gmail.com>
2024-07-16 17:47:03 +0900

    Update 나누어 떨어지는 숫자 배열.py

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,22 +1,15 @@
 def solution(arr, divisor):
-    # 나누어 떨어지는 숫자를 저장할 리스트
     answer = []
     
-    # arr 리스트의 각 요소를 순회
     for num in arr:
-        # 요소가 divisor로 나누어 떨어지는지 확인
         if num % divisor == 0:
-            # 나누어 떨어지면 answer 리스트에 추가
             answer.append(num)
     
-    # 나누어 떨어지는 숫자가 없으면 -1을 반환
     if len(answer) == 0:
         return [-1]
     
-    # 나누어 떨어지는 숫자들을 오름차순으로 정렬
     answer.sort()
     
-    # 정렬된 리스트를 반환
     return answer
 
 # 테스트 예제
