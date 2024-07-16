96cc5432f6e33d816fb76c4311753d1736d55a03
alope <pablitojun@gmail.com>
2024-07-16 17:38:15 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.95 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,17 +1,25 @@
 def solution(arr, divisor):
-    # divisor로 나누어 떨어지는 요소들만 추출
-    answer = [num for num in arr if num % divisor == 0]
+    # 나누어 떨어지는 숫자를 저장할 리스트
+    answer = []
     
-    # 나누어 떨어지는 요소가 하나도 없는 경우
-    if not answer:
+    # arr 리스트의 각 요소를 순회
+    for num in arr:
+        # 요소가 divisor로 나누어 떨어지는지 확인
+        if num % divisor == 0:
+            # 나누어 떨어지면 answer 리스트에 추가
+            answer.append(num)
+    
+    # 나누어 떨어지는 숫자가 없으면 -1을 반환
+    if len(answer) == 0:
         return [-1]
     
-    # 요소들을 오름차순으로 정렬
+    # 나누어 떨어지는 숫자들을 오름차순으로 정렬
     answer.sort()
     
+    # 정렬된 리스트를 반환
     return answer
 
-# 테스트
-print(solution([5, 9, 7, 10], 5))  # [5, 10]
-print(solution([2, 36, 1, 3], 1))  # [1, 2, 3, 36]
-print(solution([3, 2, 6], 10))     # [-1]
+# 테스트 예제
+print(solution([5, 9, 7, 10], 5))  # [5, 10] 출력 예상
+print(solution([2, 36, 1, 3], 1))  # [1, 2, 3, 36] 출력 예상
+print(solution([3, 2, 6], 10))     # [-1] 출력 예상
