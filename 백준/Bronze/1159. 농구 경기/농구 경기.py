cce9f5d9e2f9e7af4d8d5fd0efb77200aa103b21
alope <pablitojun@gmail.com>
2025-01-05 11:20:44 +0900

    [Bronze II] Title: 농구 경기, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.py
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -1,19 +1,15 @@
 from collections import Counter
 
-# 선수 수 입력
 number = int(input())
 name_list = []
 
-# 선수들의 성 첫 글자 입력
 for _ in range(number):
     name = input()
     name_list.append(name[0])
 
-# 성 첫 글자 카운트
 word_count = Counter(name_list)
-most_used = word_count.most_common()
+most_used = Counter.most_common(word_count)
 
-# 기존 로직 유지
 result = []
 flag = 0
 
@@ -22,8 +18,7 @@ for i in range(len(most_used)):
         result.append(most_used[i][0])
         flag = 1
 
-# 결과 출력
 if flag == 0:
     print("PREDAJA")
-else:
-    print("".join(sorted(result)))  # 알파벳 순 정렬 후 출력
+elif flag == 1:
+    print("".join(sorted(result)))
